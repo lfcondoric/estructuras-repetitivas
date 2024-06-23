4875d603d8e5c4460f7a93b309f14c3bcc46c8b1
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-06-22 23:28:33 -0500

    Actualización 5 del ejercicio1.cpp estructuras repetitivas

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,11 +5,12 @@
 
 using namespace std;
 
-int sumaed(int e);
+int sumaed(int n);
+float talla(int n);
 
 int main(){
 	
-	int e,n,p;
+	int e,n,p,ta;
 	float pr;
 	
 	cout<<"\t\t\tSuma de edades\n";
@@ -17,19 +18,32 @@ int main(){
 	
 	p=sumaed(n);
 	pr=p/n;
+	ta=talla(n);
 	
 	cout<<"La suma de sus edades es: "<<p<<"\n";
+	cout<<"El promedio de talla de los alumnos es: "<<ta<<"\n";
 	cout<<"El promedio es: "<<pr<<"\n";
 	system("pause");
 	return 0;
 }
 
 int sumaed(int n){
-	int i=1,s=0,e;
+	int i=1,s=0,s2=0,e,t;
 	while(i<=n){
 		cout<<"Ingrese la edad del alumno "<<i<<":";cin>>e;
 		s+=e;
 		i++;
 	}
 	return (s);
+}
+
+float talla(int n){
+	float i=1,s=0,pr,e;
+	while(i<=n){
+		cout<<"Ingrese la talla del alumno "<<i<<":";cin>>e;
+		s+=e;
+		i++;
+	}
+	pr=s/n;
+	return (pr);
 }
